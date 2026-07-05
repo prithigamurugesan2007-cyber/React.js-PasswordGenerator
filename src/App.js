c7ddc717a5897c55e68532f4fddf6a14968c4527
Prithiga Murugesan <prithiga.murugesan2007@gmail.com>
2026-07-05 11:14:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import PasswordGenerator from "./components/PasswordGenerator";
+
+function App() {
+  return (
+    <React.Fragment >
+      <PasswordGenerator />
+    </React.Fragment>
+  );
+}
+
+export default App;
